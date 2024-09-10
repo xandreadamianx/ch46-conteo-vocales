91c73459c3d6da069128348995c450287f5eaddf
Andrea Damian <damian.moran.andrea@gmail.com>
2024-09-10 12:28:49 -0600

    feat: funcion que maneja el input nombre

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("conteo de vocales");
+/**
+ Encuentra el número de letras que eixten en el
+ nombre
+ @letra {string} letra a buscar
+ @nombre {string} nombre a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+function manejoDelEventoDeInputNombre(){
+	console.log ("Me estoy invocando");
+}
